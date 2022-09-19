2e242c8dda33d4b90c86b24ac828a4be7ff1d0f0
KapilRathor <kg.rathor@gmail.com>
2022-09-19 13:37:16 +0530

    Updates

--- classification.py
+++ classification.py
@@ -14,6 +14,11 @@ import matplotlib.pyplot as plt
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 
+GAMMA = 0.001
+train_frac = 0.8
+test_frac = 0.1
+dev_frac = 0.1
+
 
 # In[7]:
 
@@ -34,13 +39,22 @@ for ax, image, label in zip(axes, digits.images, digits.target):
 n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
 
+dev_test_frac = 1-train_frac
+X_train, X_dev_test, y_train, y_dev_test = train_test_split(
+    data, digits.target, test_size=dev_test_frac, shuffle=False
+)
+
+
 # Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+clf = svm.SVC()
 
 # Split data into 50% train and 50% test subsets
 X_train, X_test, y_train, y_test = train_test_split(
     data, digits.target, test_size=0.5, shuffle=False
 )
+
+hyper_params = {'gamma' : GAMMA}
+clf.set_params(**hyper_params)
 
 # Learn the digits on the train subset
 clf.fit(X_train, y_train)
